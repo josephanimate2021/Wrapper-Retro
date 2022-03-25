f424b5cb3e4e761e55c09f73fa97461bdfb23aab
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-24 22:26:27 -0400

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -86,9 +86,9 @@ module.exports = function (req, res, url) {
 	var makeZip = true;
 	switch (url.pathname) {
 		case "/goapi/getUserAssets/":
-		case "/goapi/getCommunityAssets": {
 			break;
-		}
+		case "/goapi/getCommunityAssets":
+			break;
 		case "/goapi/getUserAssetsXml/":
                         makeZip = false;
 			break;	
